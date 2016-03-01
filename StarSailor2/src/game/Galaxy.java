3707cab784867f077b6f3c6076ccb522774aedc0
mxw596 <mxw596@cca-ug04-13029.cs.bham.ac.uk>
2016-03-01 15:17:48 +0000

    changed galaxy

--- StarSailor2/src/game/Galaxy.java
+++ StarSailor2/src/game/Galaxy.java
@@ -7,7 +7,6 @@ import java.awt.event.MouseEvent;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.NoninvertibleTransformException;
 import java.awt.geom.Point2D;
-import java.awt.geom.Point2D.Double;
 
 import javax.swing.JPanel;
 
